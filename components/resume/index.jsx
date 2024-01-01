be70966f6883241d3084ea0287b324bf59ac1d74
mahesh-chand <maheshpk678@gmail.com>
2024-01-01 14:27:33 +0530

    dev

--- components/resume/index.jsx
+++ components/resume/index.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Layout from '../shared/layout'
 import { FaDownload, FaBriefcase, FaReact } from "react-icons/fa";
 
@@ -48,7 +49,7 @@ const Resume = () => {
                                     <div className="col-6">
                                         <ul className="font-sans mb-[1rem] text-[16px]">
                                             <li className='mb-[20px]'> <span className="capitalize text-gray-400">Address :</span> <span className="font-semibold text-[#666]">Jaipur</span> </li>
-                                            <li className='mb-[20px]'> <span className="capitalize text-gray-400">phone :</span> <span className="font-semibold text-[#666]">+91 9587586425</span> </li>
+                                            <li className='mb-[20px]'> <span className="capitalize text-gray-400">phone :</span> <span className="font-semibold text-[#666]">+91 8005605097</span> </li>
                                             <li className='mb-[20px]'> <span className="capitalize text-gray-400">Email :</span> <span className="font-semibold text-[#666]">mheshpk@gmail.com</span> </li>
                                             <li className='mb-[20px]'> <span className="capitalize text-gray-400">Skype :</span> <span className="font-semibold text-[#666]">mahesh prajapat</span> </li>
                                             <li className='mb-[20px]'> <span className="capitalize text-gray-400">langages :</span> <span className="font-semibold text-[#666]">Hindi, English</span> </li>
@@ -122,7 +123,6 @@ const Resume = () => {
                                 <div className='border-[1px] border-gray-300 rounded-xl h-[130px]'>
                                     <ul className='px-6 py-4 leading-[2rem] grid grid-cols-2'>
                                         <li className='flex items-center gap-2'><FaReact />React js & Next js</li>
-                                        <li className='flex items-center gap-2'><FaReact />Angular 13 & Universal,nest js</li>
                                         <li className='flex items-center gap-2'><FaReact />Vue 3 & Next js</li>
                                         <li className='flex items-center gap-2'><FaReact />Ant design & Material Ui</li>
                                     </ul>
@@ -130,49 +130,28 @@ const Resume = () => {
                             </div>
                             <div>
                                 <div className='pb-3'>
-                                    <span className='text-[20px] font-semibold text-gray-700 py-[2px] px-[20px] mb-[10px] rounded-[20px] font-semibold bg-[#eee]'>Database (Managed & Unmanaged)</span>
+                                    <span className='text-[20px] font-semibold text-gray-700 py-[2px] px-[20px] mb-[10px] rounded-[20px] font-semibold bg-[#eee]'>Backend and Database (Managed & Unmanaged)</span>
                                 </div>
-                                <div className='border-[1px] border-gray-300 rounded-xl'>
+                                <div className='border-[1px] border-gray-300 rounded-xl  h-[160px]'>
                                     <ul className='px-6 py-4 leading-[2rem] grid grid-cols-2'>
+                                        <li className='flex items-center gap-2'><FaReact />Node js,Express js & Fastify</li>
+                                        <li className='flex items-center gap-2'><FaReact />PHP & Laravel</li>
+                                        <li className='flex items-center gap-2'><FaReact />Wordpress</li>
                                         <li className='flex items-center gap-2'><FaReact />MySql</li>
-                                        <li className='flex items-center gap-2'><FaReact />Postgre Sql</li>
-                                        <li className='flex items-center gap-2'><FaReact />AWS Dynamo DB</li>
-                                        <li className='flex items-center gap-2'><FaReact />AWS Document DB</li>
                                         <li className='flex items-center gap-2'><FaReact />Mongo DB</li>
                                     </ul>
                                 </div>
                             </div>
                             <div>
                                 <div className='pb-3'>
-                                    <span className='text-[20px] font-semibold text-gray-700 py-[2px] px-[20px] mb-[10px] rounded-[20px] font-semibold bg-[#eee]'>Backend & Server Side</span>
-                                </div>
-                                <div className='border-[1px] border-gray-300 rounded-xl'>
-                                    <ul className='px-6 py-4 leading-[2rem] grid grid-cols-2'>
-                                        <li className='flex items-center gap-2'><FaReact />Node js,Express js & Fastify</li>
-                                        <li className='flex items-center gap-2'><FaReact />PHP & Laravel</li>
-                                        <li className='flex items-center gap-2'><FaReact />Wordpress</li>
-                                        <li className='flex items-center gap-2'><FaReact />Python(Basic)</li>
-                                        <li className='flex items-center gap-2'><FaReact />Java(Basic)</li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className='col-span-2'>
-                                <div className='pb-3'>
                                     <span className='text-[20px] font-semibold text-gray-700 py-[2px] px-[20px] mb-[10px] rounded-[20px] font-semibold bg-[#eee]'>Dev Oops & Project Management Tools</span>
                                 </div>
                                 <div className='border-[1px] border-gray-300 rounded-xl'>
-                                    <ul className='px-6 py-4 leading-[2rem] grid grid-cols-3 gap-x-20'>
-                                        <li className='col-span-3 flex items-center gap-2'><FaReact />AWS Cloud (EC2,Route S3,RDS,ACM,LoadBalancer,Lemda,API Gateway,Cloudfront,S3,SES & Live Streming)</li>
+                                    <ul className='px-6 py-4 leading-[2rem] grid grid-cols-2'>
+                                        <li className='col-span-2 row-span-2 flex gap-2'><FaReact className='h-7 w-7 mt-1' />AWS Cloud (EC2,Route S3,RDS,ACM,LoadBalancer,Lemda,API Gateway,Cloudfront,S3,SES & Live Streming)</li>
+                                        <li className='flex items-center gap-2'><FaReact />Github & Git</li>
                                         <li className='flex items-center gap-2'><FaReact />Zoho Sprint</li>
                                         <li className='flex items-center gap-2'><FaReact />Jira</li>
-                                        <li className='flex items-center gap-2'><FaReact />Bit Bucket</li>
-                                        <li className='flex items-center gap-2'><FaReact />Gitlab</li>
-                                        <li className='flex items-center gap-2'><FaReact />Github & Git</li>
-                                        <li className='flex items-center gap-2'><FaReact />Digital Ocean</li>
-                                        <li className='flex items-center gap-2'><FaReact />Azure</li>
-                                        <li className='flex items-center gap-2'><FaReact />Apache Kafka</li>
-                                        <li className='flex items-center gap-2'><FaReact />Kubernetis</li>
-                                        <li className='flex items-center gap-2'><FaReact />Docker</li>
                                     </ul>
                                 </div>
                             </div>
@@ -194,29 +173,20 @@ const Resume = () => {
                                                 <p className='m-4'><FaBriefcase className='text-white' /></p>
                                             </div>
                                             <div>
-                                                <span className="text-[12px] py-[1px] px-[10px] mb-[10px] rounded-[20px] font-semibold bg-[#eee] uppercase open-sans-font">Jan-2022 - Present</span>
-                                                <h5 className="mt-[7px] mb-[10px] font-semibold text-[18px] uppercase">MERN Full-Stack Developer <span className="text-[15px] font-medium text-gray-400 open-sans-font">-- Techsunset</span></h5>
-                                                <p className="text-[#666] text-[15px]">Lorem ipsum dolor sit amet, consectetur tempor incididunt ut labore adipisicing elit, </p>
-                                            </div>
-                                        </li>
-                                        <li className='flex gap-5 pr-[20px] pb-[60px] text-[#666]'>
-                                            <div className="flex items-center justify-center bg-[#72b626] rounded-full h-[40px] w-[40px]">
-                                                <p className='m-4'><FaBriefcase className='text-white' /></p>
-                                            </div>
-                                            <div>
-                                                <span className="text-[12px] py-[1px] px-[10px] mb-[10px] rounded-[20px] font-semibold bg-[#eee] uppercase open-sans-font">2021</span>
-                                                <h5 className="mt-[7px] mb-[10px] font-semibold text-[18px] uppercase">MERN Full-stack Developer Course <span className="text-[15px] font-medium text-gray-400">-- Wap Institute</span></h5>
-                                                <p className="text-[#666] text-[15px]">Lorem incididunt dolor sit amet, consectetur eiusmod dunt doldunt dol elit, tempor incididunt</p>
-                                            </div>
-                                        </li>
-                                        <li className='flex gap-5 pr-[20px] pb-[60px] text-[#666]'>
-                                            <div className="flex items-center justify-center bg-[#72b626] rounded-full h-[40px] w-[40px]">
-                                                <p className='m-4'><FaBriefcase className='text-white' /></p>
-                                            </div>
-                                            <div>
-                                                <span className="text-[12px] py-[1px] px-[10px] mb-[10px] rounded-[20px] font-semibold bg-[#eee] uppercase open-sans-font">2005 - 2013</span>
-                                                <h5 className="mt-[7px] mb-[10px] font-semibold text-[18px] uppercase">Consultant <span className="text-[15px] font-medium text-gray-400">-- Videohive</span></h5>
-                                                <p className="text-[#666] text-[15px]">Lorem ipsum dolor sit amet, tempor incididunt ut laboreconsectetur elit, sed do eiusmod tempor duntt</p>
+                                                <span className="text-[12px] py-[1px] px-[10px] mb-[10px] rounded-[20px] font-semibold bg-[#eee] uppercase open-sans-font">June-2021 - Present</span>
+                                                <h5 className="mt-[7px] mb-[10px] font-semibold text-[18px] uppercase">MERN Full-Stack Developer <span className="text-[20px] font-medium text-gray-500 open-sans-font capitalize">-- Techsunset<Link href='https://techsunset.com' target='_blank' className='text-blue-600 font-normal text-[16px]'>(https://techsunset.com)</Link></span></h5>
+                                                <h5 className="mt-[7px] mb-[10px] font-semibold text-[18px] capitalize">1.Project Title : <span className="text-[15px] font-medium text-gray-400 open-sans-font">Christschool <Link href='https://www.christschool.info/' target='_blank' className='text-blue-600 font-normal'>(https://www.christschool.info/)</Link></span></h5>
+                                                <p className="text-[#666] text-[15px]">Christ School CBSE, is the newest branch of Christ Group of Schools, Bangalore, which was begun in June 2017, and now in the current academic year has classes up to Grade IX, with 1800 students.</p>
+                                                <h5 className="mt-[7px] mb-[10px] font-semibold text-[18px] capitalize">2.Project Title : <span className="text-[15px] font-medium text-gray-400 open-sans-font">Foodaddaindia <Link href='https://www.foodaddaindia.com/' target='_blank' className='text-blue-600 font-normal'>(https://www.foodaddaindia.com/)</Link></span></h5>
+                                                <p className="text-[#666] text-[15px]">Food Adda is one of the finest places to satiate all your cheese cravings. They offer some delicious pizza sandwiches burgers drinks desserts and much more. The star of the show is their Lamboo</p>
+                                                <h5 className="mt-[7px] mb-[10px] font-semibold text-[18px] capitalize">3.Project Title : <span className="text-[15px] font-medium text-gray-400 open-sans-font">Hotel Simna <Link href='http://www.hotelsimna.com/' target='_blank' className='text-blue-600 font-normal'>(http://www.hotelsimna.com/)</Link></span></h5>
+                                                <p className="text-[#666] text-[15px]">Hotel Simna International Find a Room When you want to be our guest? No. of Rooms     Rooms Type Simna Hotel We provide the most luxurious services From our hotel, you can visit attractive places</p>
+                                                <h5 className="mt-[7px] mb-[10px] font-semibold text-[18px] capitalize">4.Project Title : <span className="text-[15px] font-medium text-gray-400 open-sans-font">Shootorder <Link href='https://www.shootorder.com/' target='_blank' className='text-blue-600 font-normal'>(https://www.shootorder.com/)</Link></span></h5>
+                                                <p className="text-[#666] text-[15px]">ShootOrder® is one of the globally awarded digital marketing agency in Hyderabad, India. We are amongst the top ranked digital marketing companies with 300+ clients across the globe driving bes</p>
+                                                <h5 className="mt-[7px] mb-[10px] font-semibold text-[18px] capitalize">5.Project Title : <span className="text-[15px] font-medium text-gray-400 open-sans-font">Aubree <Link href='https://shop.aubree.in/' target='_blank' className='text-blue-600 font-normal'>(https://shop.aubree.in/)</Link></span></h5>
+                                                <p className="text-[#666] text-[15px]">Aubree is a website that offers a variety of chocolates, cakes, and desserts made with love and passion. You can order online and get same day or next day delivery of choco-bites, cakes, tres-leches, and more in amazing flavors and designs.</p>
+                                                <h5 className="mt-[7px] mb-[10px] font-semibold text-[18px] capitalize">6.Project Title : <span className="text-[15px] font-medium text-gray-400 open-sans-font">Aquastore <Link href='https://www.aquastore.in/' target='_blank' className='text-blue-600 font-normal'>(https://www.aquastore.in/)</Link></span></h5>
+                                                <p className="text-[#666] text-[15px]">AQUAStore.in deals with premium quality aquarium products including different varieties of Aquarium products, Aquarium decorative, finest fish foods, fish medicines, accessories.</p>
                                             </div>
                                         </li>
                                     </ul>
@@ -230,7 +200,17 @@ const Resume = () => {
                                                 <p className='m-4'><FaBriefcase className='text-white' /></p>
                                             </div>
                                             <div>
-                                                <span className="text-[12px] py-[1px] px-[10px] mb-[10px] rounded-[20px] font-semibold bg-[#eee] uppercase">2021</span>
+                                                <span className="text-[12px] py-[1px] px-[10px] mb-[10px] rounded-[20px] font-semibold bg-[#eee] uppercase open-sans-font">2021</span>
+                                                <h5 className="mt-[7px] mb-[10px] font-semibold text-[18px] uppercase">MERN Full-stack Developer Course <span className="text-[20px] font-medium text-gray-500 capitalize">-- <br/>Wap Institute<Link href='https://wapinstitute.com/certified' target='_blank' className='text-blue-600 font-normal text-[16px]'>(https://wapinstitute.com/)</Link></span></h5>
+                                                <p className="text-[#666] text-[15px]">Lorem incididunt dolor sit amet, consectetur eiusmod dunt doldunt dol elit, tempor incididunt</p>
+                                            </div>
+                                        </li>
+                                        <li className='flex gap-5 pr-[20px] pb-[60px] text-[#666]'>
+                                            <div className="flex items-center justify-center bg-[#72b626] rounded-full h-[40px] w-[40px]">
+                                                <p className='m-4'><FaBriefcase className='text-white' /></p>
+                                            </div>
+                                            <div>
+                                                <span className="text-[12px] py-[1px] px-[10px] mb-[10px] rounded-[20px] font-semibold bg-[#eee] uppercase">2016 - 2021</span>
                                                 <h5 className="mt-[7px] mb-[10px] font-semibold text-[18px] uppercase">Bachelor Degree <span className="text-[15px] font-medium text-gray-400">-- SS Jain Subodh PG Collage,Jaipur</span></h5>
                                                 <p className="text-[#666] text-[15px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do tempor incididunt ut labore</p>
                                             </div>
@@ -251,7 +231,7 @@ const Resume = () => {
                                             </div>
                                             <div>
                                                 <span className="text-[12px] py-[1px] px-[10px] mb-[10px] rounded-[20px] font-semibold bg-[#eee] uppercase  uppercase">2012</span>
-                                                <h5 className="mt-[7px] mb-[10px] font-semibold text-[18px] uppercase">Bachelor Degree <span className="text-[15px] font-medium text-gray-400">-- Shree Divya Public Sr. Sec. School,Jaipur</span></h5>
+                                                <h5 className="mt-[7px] mb-[10px] font-semibold text-[18px] uppercase">Secondary <span className="text-[15px] font-medium text-gray-400">-- Shree Divya Public Sr. Sec. School,Jaipur</span></h5>
                                                 <p className="text-[#666] text-[15px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor ut labore</p>
                                             </div>
                                         </li>
